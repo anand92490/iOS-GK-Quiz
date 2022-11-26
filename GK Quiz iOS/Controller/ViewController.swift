677e1ab26c100571e9530cbe2c7270c66309157d
anand92490 <anand92490@gmail.com>
2022-11-26 15:53:21 -0600

    first commit

---
+++ GK Quiz iOS/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GK Quiz iOS
+//
+//  Created by Anand Chaudhary on 11/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var questionCounter: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var progressView: UIView!
+    
+    @IBOutlet weak var flagView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
