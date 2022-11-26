c0ec47cbd9417adeae02c0f6bb672890dce43fb6
anand92490 <anand92490@gmail.com>
2022-11-26 17:17:09 -0600

    added Questions

--- GK Quiz iOS/Controller/ViewController.swift
+++ GK Quiz iOS/Controller/ViewController.swift
@@ -8,19 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var questionCounter: UILabel!
     
     @IBOutlet weak var scoreLabel: UILabel!
-    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionCounter: UILabel!
     @IBOutlet weak var progressView: UIView!
     
-    @IBOutlet weak var flagView: UIImageView!
+    // outlet for buttons
+    
+    @IBOutlet weak var optionA: UIButton!
+    @IBOutlet weak var optionB: UIButton!
+    @IBOutlet weak var optionC: UIButton!
+    @IBOutlet weak var optionD: UIButton!
+    
+    
+    //
+//    let allQuestions = QuestionBank()
+//    var questionNumber: Int = 0
+//    var score: Int = 0
+//    var selectedAnswer: Int = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func answerPressed(_ sender: UIButton) {
+        
+//        if sender.tag == 1{
+//            print("option a")
+//        }else if sender.tag == 2{
+//            print("option b")
+//        } else if sender.tag == 3{
+//            print("option c")
+//        }else{
+//            print("option d")
+//        }
+    }
+    
+    func updateQuestion(){
+//        photoView.image = UIImage(named: (allQuestions.list[questionNumber].questionImage))
+//        questionLabel.text = allQuestions.list[questionNumber].question
+//        optionA.setTitle(allQuestions.list[questionNumber].optionA, for: UIControl.State.normal)
+//        optionB.setTitle(allQuestions.list[questionNumber].optionB, for: UIControl.State.normal)
+//        optionC.setTitle(allQuestions.list[questionNumber].optionC, for: UIControl.State.normal)
+//        optionD.setTitle(allQuestions.list[questionNumber].optionD, for: UIControl.State.normal)
+//        selectedAnswer = allQuestions.list[questionNumber].correctAnswer
+//        
+        
+    }
+    
+    func updateUI(){
+        
+    }
+    
+    func restartQuiz(){
+        
+    }
 
 }
 
---
+++ GK Quiz iOS/Model /QuestionBank.swift
@@ -0,0 +1,36 @@
+//
+//  QuestionBank.swift
+//  GK Quiz iOS
+//
+//  Created by Anand Chaudhary on 11/26/22.
+//
+
+import Foundation
+
+class QuestionBank{
+    var list = [Question]()
+    
+    func int(){
+        //1
+        list.append(Question(image: "blackbird", questionText: "What is the fastest airplane in the world?", choiceA: "Lockheed SR-71 Blackbird", choiceB: "Lockheed YF-12", choiceC: "XB-70 Valkyrie", choiceD: "Bell X-2 Starbuster", answer: 1))
+        //2
+        list.append(Question(image: "cofee", questionText: "Which U.S State is the only state to grow its own coffee beans?", choiceA: "Oregon", choiceB: "Washington", choiceC: "Hawaii", choiceD: "California", answer: 3))
+        //3
+        list.append(Question(image: "stop", questionText: "What geometric shape is generally used for stop signs?", choiceA: "Decagon", choiceB: "Pentagon", choiceC: "Hexagon", choiceD: "Octagon", answer: 4))
+        //4
+        list.append(Question(image: "google", questionText: "What was the original name of the search engine \"Google\"?", choiceA: "Bing", choiceB: "BackRub", choiceC: "Ask", choiceD: "Alexandria", answer: 2))
+        //5
+        list.append(Question(image: "juniper", questionText: "What alcoholic drink is mainly made from juniper berries?", choiceA: "Vodka", choiceB: "Rum", choiceC: "Gin", choiceD: "Tequila", answer: 3))
+        //6
+        list.append(Question(image: "london", questionText: "Which of these cities is NOT in England?", choiceA: "Edinburgh", choiceB: "Birmingham", choiceC: "Liverpool", choiceD: "Bristol", answer: 1))
+        //7
+        list.append(Question(image: "batterydura", questionText: "The term \"battery\" to describe an electrical storage device was coined by?", choiceA: "Nikola Tesla", choiceB: "Thomas Edison", choiceC: "Benjamin Franklin", choiceD: "Ada Lovelace", answer: 1))
+        //8
+        list.append(Question(image: "caterpiller", questionText: "How many eyes do caterpillars have?", choiceA: "Eight", choiceB: "four", choiceC: "two", choiceD: "Twelve", answer: 4))
+        //9
+        list.append(Question(image: "soviet", questionText: "Which country was NOT part of the Soviet Union?", choiceA: "Azerbaijan", choiceB: "Romania", choiceC: "Kyrgyzstan", choiceD: "Estonia", answer: 2))
+        //10
+        list.append(Question(image: "peregrine_falcon", questionText: "What is the world's fastest bird?", choiceA: "The Peregrine Falcon", choiceB: "Golden Eagle", choiceC: "Pigeon", choiceD: "Frigate Bird", answer: 1))
+        
+    }
+}
