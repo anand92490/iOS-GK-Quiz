e451904a9c31d7c241d54a94ef66ecc4efb5535d
anand92490 <anand92490@gmail.com>
2022-12-05 16:30:51 -0600

    new questions and assets

--- GK Quiz iOS/Model /QuestionBank.swift
+++ GK Quiz iOS/Model /QuestionBank.swift
@@ -12,25 +12,25 @@ class QuestionBank{
 
      init(){
         //1
-        list.append(Question(image: "blackbird", questionText: "What is the fastest airplane in the world?", choiceA: "Lockheed SR-71 Blackbird", choiceB: "Lockheed YF-12", choiceC: "XB-70 Valkyrie", choiceD: "Bell X-2 Starbuster", answer: 1))
+        list.append(Question(image: "hashtag", questionText: "What is another name for hashtag?", choiceA: "Octothorpe", choiceB: "Pentothorpe", choiceC: "Hasithorpe", choiceD: "Overthorpe", answer: 1))
         //2
-        list.append(Question(image: "coffee", questionText: "Which U.S State is the only state to grow its own coffee beans?", choiceA: "Oregon", choiceB: "Washington", choiceC: "Hawaii", choiceD: "California", answer: 3))
+        list.append(Question(image: "applePie", questionText: "Where does american apple pie originate from?", choiceA: "England", choiceB: "France", choiceC: "Canada", choiceD: "Ohio", answer: 1))
         //3
-        list.append(Question(image: "stop", questionText: "What geometric shape is generally used for stop signs?", choiceA: "Decagon", choiceB: "Pentagon", choiceC: "Hexagon", choiceD: "Octagon", answer: 4))
+        list.append(Question(image: "phosphate", questionText: "Which country has the largest phosphate reserves?", choiceA: "China", choiceB: "Russia", choiceC: "Indonesia", choiceD: "Morocco", answer: 4))
         //4
-        list.append(Question(image: "google", questionText: "What was the original name of the search engine \"Google\"?", choiceA: "Bing", choiceB: "BackRub", choiceC: "Ask", choiceD: "Alexandria", answer: 2))
+        list.append(Question(image: "africa", questionText: "Which African country was formerly known as Abyssinia?", choiceA: "Ethiopia", choiceB: "Eritrea", choiceC: "Tanzania", choiceD: "Somalia", answer: 1))
         //5
-        list.append(Question(image: "juniper", questionText: "What alcoholic drink is mainly made from juniper berries?", choiceA: "Vodka", choiceB: "Rum", choiceC: "Gin", choiceD: "Tequila", answer: 3))
+        list.append(Question(image: "president", questionText: "How many presidents does bosnia have?", choiceA: "2", choiceB: "3", choiceC: "4", choiceD: "6", answer: 2))
         //6
-        list.append(Question(image: "london", questionText: "Which of these cities is NOT in England?", choiceA: "Edinburgh", choiceB: "Birmingham", choiceC: "Liverpool", choiceD: "Bristol", answer: 1))
+        list.append(Question(image: "panda", questionText: "What is a group of pandas known as?", choiceA: "An Embarrassment", choiceB: "Nursery", choiceC: "Sloth", choiceD: "Pack", answer: 1))
         //7
-        list.append(Question(image: "batterydura", questionText: "The term \"battery\" to describe an electrical storage device was coined by?", choiceA: "Nikola Tesla", choiceB: "Thomas Edison", choiceC: "Benjamin Franklin", choiceD: "Ada Lovelace", answer: 3))
+        list.append(Question(image: "sushi", questionText: "Where did sushi originate from? ", choiceA: "Japan", choiceB: "Vietnam", choiceC: "China", choiceD: "Korea", answer: 3))
         //8
-        list.append(Question(image: "caterpillar", questionText: "How many eyes do caterpillars have?", choiceA: "Eight", choiceB: "four", choiceC: "two", choiceD: "Twelve", answer: 4))
+        list.append(Question(image: "ravens", questionText: "A group of ravens is known as what?", choiceA: "Unkindness", choiceB: "Ostentation", choiceC: "Gaggle", choiceD: "Pride", answer: 1))
         //9
-        list.append(Question(image: "soviet", questionText: "Which country was NOT part of the Soviet Union?", choiceA: "Azerbaijan", choiceB: "Romania", choiceC: "Kyrgyzstan", choiceD: "Estonia", answer: 2))
+        list.append(Question(image: "vomit", questionText: "Who invented the word vomit?", choiceA: "Edgar Allen Poe", choiceB: "William Shakespeare", choiceC: "Robert Frost", choiceD: "Charles Dickens", answer: 2))
         //10
-        list.append(Question(image: "peregrine_falcon", questionText: "What is the world's fastest bird?", choiceA: "The Peregrine Falcon", choiceB: "Golden Eagle", choiceC: "Pigeon", choiceD: "Frigate Bird", answer: 1))
+        list.append(Question(image: "octopus", questionText: "How many hearts does an octopus have? ", choiceA: "3", choiceB: "2", choiceC: "1", choiceD: "0", answer: 1))
 
     }
 }
